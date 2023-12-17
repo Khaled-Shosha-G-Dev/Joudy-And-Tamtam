b3dcba767d604d1ecba8c5f4c17540ba32248e6d
Khaled Hossam Ahmed <khaledhossamG.dev@gmail.com>
2023-12-17 22:20:40 +0200

    Add new Animations
    1-adding animation(punch(1,2,3,4))
    2-adding animation(Kick(1,2,3,4))
    3-adding animation(Jumping)
    4-adding a attack ponits when fight enemy
    5-deleted an extra files

--- Assets/Scripts/PlayerScripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerScripts/PlayerAttack.cs
@@ -6,6 +6,13 @@ public enum ComboAttack
 {
     Kick1=1,
     Kick2,
+    Kick3,
+    Kick4,
+    Punch1,
+    Punch2,
+    Punch3,
+    Punch4,
+
 }
 
 public class PlayerAttack : MonoBehaviour
@@ -25,13 +32,24 @@ public class PlayerAttack : MonoBehaviour
 
     private void ComboPunchs()
     {
+        int randomPunch = Random.Range(5,9);
         if (Input.GetKeyDown(KeyCode.F))
-            playerAnim.SetTrigger("Punch1");
+        {
+            ComboAttack selectedAttack = (ComboAttack)randomPunch;
+            if (selectedAttack == ComboAttack.Punch1)
+                playerAnim.SetTrigger("Punch1");
+            else if (selectedAttack == ComboAttack.Punch2)
+                playerAnim.SetTrigger("Punch2");
+            else if (selectedAttack == ComboAttack.Punch3)
+                playerAnim.SetTrigger("Punch3");
+            else if (selectedAttack == ComboAttack.Punch4)
+                playerAnim.SetTrigger("Punch4");
+        }
     }
     private void ComboKicks()
     {
 
-        int randomKick = Random.Range(1, 3);
+        int randomKick = Random.Range(1, 5);
         if (Input.GetKeyDown(KeyCode.G))
         {
             ComboAttack selectedAttack = (ComboAttack)randomKick;
@@ -39,6 +57,10 @@ public class PlayerAttack : MonoBehaviour
                 playerAnim.SetTrigger("Kick1");
             else if (selectedAttack == ComboAttack.Kick2)
                 playerAnim.SetTrigger("Kick2");
+            else if (selectedAttack == ComboAttack.Kick3)
+                playerAnim.SetTrigger("Kick3");
+            else if (selectedAttack == ComboAttack.Kick4)
+                playerAnim.SetTrigger("Kick4");
         }
 
     }
